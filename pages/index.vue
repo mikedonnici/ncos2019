630ec914a303eed6efac8ad961c5e980c918acc9
Mike Donnici <michael@mesa.net.au>
2019-06-10 21:01:50 +1000

    add session info

--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,7 @@
   <section class="section">
     <div class="container">
       <div class="content">
-
         <img src="/NCOS.png" alt="NCOS 2019">
-
 
         <h1 class="title is-1">Northern Community of Schools</h1>
         <h2 class="subtitle is-3">
@@ -63,9 +61,70 @@
           <li>What training is available from the ADA for teachers?</li>
         </ul>
 
-        <p class="notification blockquote"><i>‘Many students in schools go unidentified and unassisted, often it is not until they are failing 
-          in literacy areas, that someone, wonders about dyslexia. It is never too late to get identified 
-          and assisted</i>‘ – ADA</p>
+        <p class="notification blockquote">
+          <i>
+            ‘Many students in schools go unidentified and unassisted, often it is
+            not until they are failing in literacy areas, that someone, wonders about dyslexia. It is never too late to get identified
+            and assisted
+          </i>‘ – ADA
+        </p>
+        <p class="notification blockquote">
+          “Science has moved forward at a rapid pace so that we now possess the data to reliably define
+          dyslexia … For the student, the knowledge that he is dyslexic is empowering … [It provides him]
+          with self-understanding and self-awareness of what he has and what he needs to do in order to
+          succeed.” - Sally Shaywitz, M.D., co-director of Yale Center for Dyslexia and Creativity
+        </p>
+
+        <h3>The Trauma Informed classroom</h3>
+        <p>This is a two hour session you must select both session 1 and 2</p>
+
+        <p class="notification is-info">
+          Julie Mehic (Head Teacher Wellbeing), Kate Burrett (Head teacher STC), Bomaderry High School
+          <br>Intended audience: all staff
+          <br>Session: both session 1 and session 2
+        </p>
+
+        <p>
+          In today’s changing and complex world, many primary and secondary school students encounter
+          daily challenges that can impact their success at school. These range from ongoing stress and
+          lack of family support to a specific traumatic event.
+        </p>
+        <p>
+          This session builds on from last year's session called 'Trauma Informed Practice'; however,
+          you will benefit from this session whether or not you attended last year. This session will
+          provide you with a toolkit of activities, ideas and strategies to use in your classroom.
+        </p>
+
+        <hr>
+
+        <p class="notification">
+          One Hour sessions. Staff can choose 2 x one hour sessions
+          <br>Session One -11:15 - 12:15
+          <br>Session Two- 1:00 - 2:00pm
+        </p>
+
+        <h3>Speaking and Working Scientifically</h3>
+
+        <p class="notification is-info">
+          Christine Gates, Head Teacher Science Bomaderry High School
+          <br>Intended audience: K-6 and 7-12 support
+          <br>Session One - 11:15am - 12:15pm
+        </p>
+
+        <p>
+          This hands-on workshop is designed to easily show you how Science can be performed in a
+          meaningful way using basic recycled and readily available resources. You will be given the
+          chance to undertake a variety of experiments that can be modified and used for all age groups
+          and cost very little. The experiments are designed to link with the Stage 1-3 syllabus, but could
+          also be utilised in Stage 4-6 support classes. We will also discuss the correct metalanguage
+          to be used in teaching Science along with how to conduct a student focussed, inquiry based
+          fair test that will be useful for schools who run a Science Fair.
+        </p>
+
+
+      
+
+
       </div>
     </div>
   </section>
