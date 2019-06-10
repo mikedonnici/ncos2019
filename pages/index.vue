b9862e91c29ea3237326a3a7ad09aca89d4f705e
Mike Donnici <michael@mesa.net.au>
2019-06-10 20:39:16 +1000

    initial

---
+++ pages/index.vue
@@ -0,0 +1,81 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <div class="content">
+
+        <img src="/NCOS.png" alt="NCOS 2019">
+
+
+        <h1 class="title is-1">Northern Community of Schools</h1>
+        <h2 class="subtitle is-3">
+          Session Information for SDD
+          <strong>22nd of July 2019</strong>
+        </h2>
+
+        <p class="notification">
+          Session 1: 11:15am to 12:15pm
+          <br>Session 2: 1:00pm to 2:00pm
+          <br>Audience: all staff - K-6 teachers
+          <br>
+        </p>
+
+        <h3>Dyslexia</h3>
+
+        <p>This is a two hour session you must select both session 1 and 2</p>
+
+        <p class="notification is-info">
+          Presenter: Qualified ADA assessor
+          <br>Audience: all staff
+          <br>Session: Both Session 1 and Session 2
+        </p>
+
+        <p>
+          This is an informative session relevant to every classroom in both secondary and
+          primary schools. Imagine the difference we could make to student results if we could
+          identify and assist our dyslexic students. Many of these students are ‘left behind’ because
+          of a lack of understanding and a lack of trained dyslexic teachers. This session is a start
+          to understanding the dyslexic students in your classroom – most of whom will not have been
+          formally assessed.
+        </p>
+
+        <p>
+          The Australian Dyslexia Association (ADA) states that dyslexia is estimated to affect
+          approximately 10% of the Australian population. However, they believe this may be a conservative
+          estimate as many individuals in Australia are left unidentified. In other English speaking
+          countries, the figures are an astounding 20% (UK. USA and Canada.)
+        </p>
+
+        <p>
+          This means one in five of our students could be affected by dyslexia. Dyslexia is the most
+          common cause of reading, writing and spelling difficulties. Unfortunately, many classroom teachers
+          are not aware of the signs that indicate dyslexia, where students can be assessed, specific
+          classroom strategies for these students, or the training teachers can undertake to teach dyslexic
+          students.
+        </p>
+
+        <p>Bomaderry High School has arranged for a qualified ADA assessor to present a two hour session on:</p>
+
+        <ul>
+          <li>What is dyslexia?</li>
+          <li>What are the indicators you may be teaching a dyslexic student?</li>
+          <li>Where can a student be assessed?</li>
+          <li>What classroom accommodations need to be made for dyslexic students to succeed?</li>
+          <li>What training is available from the ADA for teachers?</li>
+        </ul>
+
+        <p class="notification blockquote"><i>‘Many students in schools go unidentified and unassisted, often it is not until they are failing 
+          in literacy areas, that someone, wonders about dyslexia. It is never too late to get identified 
+          and assisted</i>‘ – ADA</p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  components: {}
+}
+</script>
+
+<style>
+</style>
